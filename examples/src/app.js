ed9d41fc42d6d7682f87367dc1b763efc6d8a793
Falenos <georgis.kakavas@gmail.com>
2017-08-01 11:47:17 +0300

    Changed filenames. Removed all the stuff from the constructor.

--- examples/src/app.js
+++ examples/src/app.js
@@ -2,20 +2,20 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import MinimalReactTextArea from './components/MinimalReactText';
+import MinimalReactTextarea from './components/MinimalReactText';
 
 ReactDOM.render(
   <form noValidate>
 	<div>
     <h3>Normal Textarea</h3>
-      <MinimalReactTextArea label="Label" maxRows={5} rows={1} />
+      <MinimalReactTextarea label="Label" maxRows={5} rows={1} />
 
     <h3>Large Textarea</h3>
-      <MinimalReactTextArea label="Label" isRequired={true} disabled={true} value={"Lalakia"}/>
+      <MinimalReactTextarea label="Label" isRequired={true} isDisabled={true} value={"Lalakia"}/>
 
     <h3>Dark Theme</h3>
     <div className="dark-background">
-      <MinimalReactTextArea label="Label" isRequired={true} theme="dark"/>
+      <MinimalReactTextarea label="Label" isRequired={true} theme="dark"/>
     </div>
 	</div>
     <button type="submit">Submit</button>
